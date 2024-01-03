a469e19d543b0263a800d7b8332b338b7a10588d
francastell <fsophisco@yahoo.com>
2024-01-03 12:05:50 -0500

    code linting for tests, README udpates

--- tests/controllers/test_genesets.py
+++ tests/controllers/test_genesets.py
@@ -1,15 +1,13 @@
-""" Tests for geneset API"""
+"""Tests for geneset API."""
 
 import importlib.resources
 import json
-from unittest.mock import Mock
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 import psycopg
 import pytest
 from fastapi.testclient import TestClient
-
-from geneweaver.api.dependencies import full_user, cursor
+from geneweaver.api.dependencies import cursor, full_user
 from geneweaver.api.main import app
 
 client = TestClient(app)
@@ -24,31 +22,34 @@ response_mock = Mock()
 response_mock.status_code = 200
 response_mock.json.return_value = test_data
 
+
 ### Mock dependencies
 def mock_full_user() -> Mock:
-    """ User auth mock"""
+    """User auth mock."""
     m1 = Mock()
     return m1.AsyncMock()
 
+
 def mock_cursor() -> psycopg.Cursor:
-    """ DB cursor mock"""
+    """DB cursor mock."""
     m2 = Mock()
     return m2.AsyncMock()
 
-app.dependency_overrides.update(
-    {full_user: mock_full_user, cursor: mock_cursor}
-)
 
-@patch.object(client, 'get', return_value=response_mock)
+app.dependency_overrides.update({full_user: mock_full_user, cursor: mock_cursor})
+
+
+@patch.object(client, "get", return_value=response_mock)
 def test_get_geneset(mock_client):
-    """ test get request for geneset ID"""
+    """Test get request for geneset ID."""
     response = client.get("/api/genesets/1234")
     assert response.status_code == 200
 
-@patch('geneweaver.api.services.geneset.get_geneset')
-@patch('geneweaver.api.services.geneset.is_geneset_readable_by_user')
+
+@patch("geneweaver.api.services.geneset.get_geneset")
+@patch("geneweaver.api.services.geneset.is_geneset_readable_by_user")
 def test_get_geneset_response(mock_genset_is_readable, mock_get_genenset):
-    """ test get geneset ID data response"""
+    """Test get geneset ID data response."""
     mock_genset_is_readable.return_value = True
     mock_get_genenset.return_value = test_data
 
@@ -56,23 +57,21 @@ def test_get_geneset_response(mock_genset_is_readable, mock_get_genenset):
     assert response.status_code == 200
     assert response.json() == test_data
 
-@patch('geneweaver.api.services.geneset.db_is_readable')
-def test_get_geneset_forbidden(mock_genset_is_readable):
-    """ test forbidden response"""
 
+@patch("geneweaver.api.services.geneset.db_is_readable")
+def test_get_geneset_forbidden(mock_genset_is_readable):
+    """Test forbidden response."""
     mock_genset_is_readable.return_value = False
     response = client.get("/api/genesets/1234")
 
-    assert response.json() == {'detail': 'Forbidden'}
+    assert response.json() == {"detail": "Forbidden"}
     assert response.status_code == 403
 
-@patch('geneweaver.api.services.geneset.db_is_readable')
-def test_get_geneset_unexpected_error(mock_genset_is_readable):
-    """ test unexpected error response"""
 
+@patch("geneweaver.api.services.geneset.db_is_readable")
+def test_get_geneset_unexpected_error(mock_genset_is_readable):
+    """Test unexpected error response."""
     mock_genset_is_readable.side_effect = Exception
 
-    with pytest.raises(Exception):
-        response = client.get("/api/genesets/1234")
-        assert response.json() == {'detail': 'Unexpected error'}
-        assert response.status_code == 500
+    with pytest.raises(expected_exception=Exception):
+        client.get("/api/genesets/1234")
--- tests/services/test_genset.py
+++ tests/services/test_genset.py
@@ -1,11 +1,10 @@
-""" Tests for geneset Service"""
+"""Tests for geneset Service."""
 
 import importlib.resources
 import json
 from unittest.mock import patch
 
 import pytest
-
 from geneweaver.api.controller import message
 from geneweaver.api.services import geneset
 
@@ -15,62 +14,58 @@ str_json = importlib.resources.read_text("tests.data", "response_geneset_1234.js
 # returns JSON string as a dictionary
 test_data = json.loads(str_json)
 
-@patch('geneweaver.api.services.geneset.db_geneset')
-@patch('geneweaver.api.services.geneset.db_geneset_value')
-@patch('geneweaver.api.services.geneset.is_geneset_readable_by_user')
-def test_get_geneset(mock_genset_readable_func, mock_db_geneset, mock_db_genset_value):
-    """ test basic get geneset by ID"""
 
+@patch("geneweaver.api.services.geneset.db_geneset")
+@patch("geneweaver.api.services.geneset.db_geneset_value")
+@patch("geneweaver.api.services.geneset.is_geneset_readable_by_user")
+def test_get_geneset(mock_genset_readable_func, mock_db_geneset, mock_db_genset_value):
+    """Test basic get geneset by ID."""
     mock_genset_readable_func.return_value = True
     mock_db_geneset.by_id.return_value = {}
     mock_db_genset_value.by_id.return_value = {}
     response = geneset.get_geneset(1234, None, None)
-    assert response.get('error') is None
+    assert response.get("error") is None
 
 
-@patch('geneweaver.api.services.geneset.is_geneset_readable_by_user')
+@patch("geneweaver.api.services.geneset.is_geneset_readable_by_user")
 def test_get_geneset_no_user_access(mock_genset_readable_func):
-    """ test get geneset by ID with no user access"""
-
+    """Test get geneset by ID with no user access."""
     mock_genset_readable_func.return_value = False
     response = geneset.get_geneset(1234, None, None)
-    assert response.get('error') is True
-    assert response.get('message') == message.ACCESS_FORBIDEN
+    assert response.get("error") is True
+    assert response.get("message") == message.ACCESS_FORBIDEN
 
 
-@patch('geneweaver.api.services.geneset.db_geneset')
-@patch('geneweaver.api.services.geneset.db_geneset_value')
-@patch('geneweaver.api.services.geneset.is_geneset_readable_by_user')
-def test_get_geneset_returned_values(mock_genset_readable_func,
-                                     mock_db_genset_value,
-                                     mock_db_geneset):
-    """ test get geneset by ID data response structure"""
-
+@patch("geneweaver.api.services.geneset.db_geneset")
+@patch("geneweaver.api.services.geneset.db_geneset_value")
+@patch("geneweaver.api.services.geneset.is_geneset_readable_by_user")
+def test_get_geneset_returned_values(
+    mock_genset_readable_func, mock_db_genset_value, mock_db_geneset
+):
+    """Test get geneset by ID data response structure."""
     mock_genset_readable_func.return_value = True
-    mock_db_geneset.by_id.return_value = test_data.get('geneset')
-    mock_db_genset_value.by_geneset_id.return_value = test_data.get('geneset_values')
+    mock_db_geneset.by_id.return_value = test_data.get("geneset")
+    mock_db_genset_value.by_geneset_id.return_value = test_data.get("geneset_values")
     response = geneset.get_geneset(1234, None, None)
 
-    assert response.get('genset') == test_data['geneset']
-    assert response.get('geneset_values') == test_data['geneset_values']
+    assert response.get("genset") == test_data["geneset"]
+    assert response.get("geneset_values") == test_data["geneset_values"]
 
 
-@patch('geneweaver.api.services.geneset.db_is_readable')
-@patch('geneweaver.api.services.geneset.User')
+@patch("geneweaver.api.services.geneset.db_is_readable")
+@patch("geneweaver.api.services.geneset.User")
 def test_is_redable_by_user(mock_user, mock_genset_is_readable):
-    """ test is geneset ID readable by passed user"""
-
+    """Test is geneset ID readable by passed user."""
     mock_genset_is_readable.return_value = True
     response = geneset.is_geneset_readable_by_user(1234, mock_user, None)
     assert response is True
 
 
-@patch('geneweaver.db.geneset.is_readable')
-@patch('geneweaver.api.services.geneset.User')
+@patch("geneweaver.db.geneset.is_readable")
+@patch("geneweaver.api.services.geneset.User")
 def test_is_redable_by_user_error(mock_user, mock_genset_is_readable):
-    """ test is geneset ID readable with server error"""
-
+    """Test is geneset ID readable with server error."""
     mock_genset_is_readable.sideEffect = Exception
 
-    with pytest.raises(Exception):
+    with pytest.raises(expected_exception=Exception):
         geneset.is_geneset_readable_by_user(1234, mock_user, None)
