9d74b0aeb1bc951b1a9e280afeb72680c609e6f5
Alexander Berger <alexander.berger@jax.org>
2024-01-16 15:48:53 -0500

    Adding register sso user in api full_user dependency

--- src/geneweaver/api/dependencies.py
+++ src/geneweaver/api/dependencies.py
@@ -6,8 +6,10 @@ import psycopg
 from fastapi import Depends
 from geneweaver.api.core.config import settings
 from geneweaver.api.core.security import Auth0, UserInternal
-from geneweaver.db.user import by_sso_id
+from geneweaver.db import user as db_user
 from psycopg.rows import dict_row
+
+from geneweaver.api.core.exceptions import AuthenticationMismatch
 
 auth = Auth0(
     domain=settings.AUTH_DOMAIN,
@@ -30,6 +32,15 @@ def full_user(
     cursor: Cursor = Depends(cursor),
     user: UserInternal = Depends(auth.get_user_strict),
 ) -> UserInternal:
-    """Get the full user object."""
-    user.id = by_sso_id(cursor, user.sso_id)[0]["usr_id"]
+    """Get the full user object."""''
+    try:
+        user.id = db_user.by_sso_id_and_email(cursor, user.sso_id, user.email)[0]["usr_id"]
+    except IndexError:
+        if db_user.sso_id_exists(cursor, user.sso_id):
+            raise AuthenticationMismatch(detail="Email and SSO ID Mismatch. Please contact and administrator.")
+        elif db_user.email_exists(cursor, user.email):
+            user.id = db_user.link_user_id_with_sso_id(cursor, user.id, user.sso_id)
+        else:
+            user.id = db_user.create_sso_user(cursor, user.name, user.email, user.sso_id)
+
     yield user
