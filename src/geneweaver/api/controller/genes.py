7c8df615b8a657d4fd217ed3e150657a62b4808f
francastell <fsophisco@yahoo.com>
2024-01-22 15:57:37 -0500

    g3-122 intial commit

---
+++ src/geneweaver/api/controller/genes.py
@@ -0,0 +1,30 @@
+"""Endpoints related to genes."""
+from typing import Optional
+
+from fastapi import APIRouter, Depends
+from geneweaver.api import dependencies as deps
+from geneweaver.api.schemas.apimodels import GeneIdMappingReq, GeneIdMappingResp
+from geneweaver.api.services import genes as genes_service
+
+router = APIRouter(prefix="/genes")
+
+
+@router.post("/homologous_ids", response_model=GeneIdMappingResp)
+def get_related_gene_ids(
+    gene_id_mapping: GeneIdMappingReq,
+    cursor: Optional[deps.Cursor] = Depends(deps.cursor),
+) -> GeneIdMappingResp:
+    """Get homologous gene ids given list of gene ids."""
+    response = genes_service.get_gene_mapping(
+        cursor,
+        gene_id_mapping.source_ids,
+        gene_id_mapping.target_gene_id_type,
+        gene_id_mapping.source_gene_id_type,
+        gene_id_mapping.target_species,
+        gene_id_mapping.source_species,
+    )
+
+    resp_id_map = response.get("ids_map")
+    gene_id_mapping_resp = GeneIdMappingResp(gene_ids_map=resp_id_map)
+
+    return gene_id_mapping_resp
---
+++ src/geneweaver/api/services/genes.py
@@ -0,0 +1,47 @@
+"""Service methods for genes."""
+
+from typing import Iterable, Optional
+
+from fastapi.logger import logger
+from geneweaver.core.enum import GeneIdentifier, Species
+from geneweaver.db.gene import get_homolog_ids
+from psycopg import Cursor
+
+
+def get_gene_mapping(
+    cursor: Cursor,
+    gene_id_list: Iterable,
+    target_gene_id_type: GeneIdentifier,
+    source_gene_id_type: Optional[GeneIdentifier],
+    target_species: Optional[Species],
+    source_species: Optional[Species],
+) -> dict:
+    """Get homologus gene identifier mappings.
+
+    Get gene id mappings based on source/target GeneIdentifier id and
+    source/target species.
+
+    @param cursor: DB Cursor
+    @param gene_id_list: list of ids to search
+    @param source_gene_id_type: source gene identifier to translate from
+    @param target_gene_id_type: target gene identifier type to search for
+    @param target_species: target species identifier to translate to
+    @param source_species: source species identifier to search for
+    @return: dictionary with id mappings.
+    """
+    ids_map = None
+    try:
+        ids_map = get_homolog_ids(
+            cursor,
+            gene_id_list,
+            target_gene_id_type,
+            source_gene_id_type,
+            target_species,
+            source_species,
+        )
+
+    except Exception as err:
+        logger.error(err)
+        raise err
+
+    return {"ids_map": ids_map}
