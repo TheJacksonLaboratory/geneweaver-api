4b14085d487bcfec84ad14e07c0f746a1cdf7747
francastell <fsophisco@yahoo.com>
2024-02-06 16:34:37 -0500

    GW-153 publication by geneset id

--- tests/controllers/test_genesets.py
+++ tests/controllers/test_genesets.py
@@ -3,10 +3,12 @@ from unittest.mock import patch
 
 import pytest
 
-from tests.data import test_geneset_data
+from tests.data import test_geneset_data, test_publication_data
 
 geneset_by_id_resp = test_geneset_data.get("geneset_by_id_resp")
 geneset_w_gene_id_type_resp = test_geneset_data.get("geneset_w_gene_id_type_resp")
+geneset_metadata_w_pub_info = test_geneset_data.get("geneset_metadata_w_pub_info")
+publication_by_id_resp = test_publication_data.get("publication_by_id")
 
 
 @patch("geneweaver.api.services.geneset.get_geneset")
@@ -79,3 +81,63 @@ def test_invalid_gene_type_id(mock_service_get_geneset_w_gene_id_type, client):
 
     assert "ctx" in response.json()["detail"][0]
     assert response.status_code == 422
+
+
+@patch("geneweaver.api.services.geneset.get_geneset_metadata")
+@patch("geneweaver.api.services.geneset.is_geneset_readable_by_user")
+def test_get_geneset_metadata(mock_genset_is_readable, mock_get_genenset, client):
+    """Test get geneset metadata."""
+    mock_genset_is_readable.return_value = True
+    mock_get_genenset.return_value = geneset_by_id_resp
+    response = client.get("/api/genesets/1234/metadata")
+
+    assert response.status_code == 200
+    assert response.json() == geneset_by_id_resp
+
+
+@patch("geneweaver.api.services.geneset.get_geneset_metadata")
+@patch("geneweaver.api.services.geneset.is_geneset_readable_by_user")
+def test_get_geneset_metadata_w_publication(
+    mock_genset_is_readable, mock_get_genenset, client
+):
+    """Test get geneset ID data response."""
+    mock_genset_is_readable.return_value = True
+    mock_get_genenset.return_value = geneset_metadata_w_pub_info
+
+    response = client.get("/api/genesets/1234/metadata?include_pub_info=true")
+    assert response.status_code == 200
+    assert response.json() == geneset_metadata_w_pub_info
+
+
+@patch("geneweaver.api.services.geneset.get_geneset_metadata")
+@patch("geneweaver.api.services.geneset.is_geneset_readable_by_user")
+@patch("geneweaver.api.services.publications.get_publication")
+def test_publication_for_geneset(
+    mock_pub_service_call, mock_genset_is_readable, mock_get_genenset_metadata, client
+):
+    """Test valid url request to get publication for a geneset."""
+    mock_genset_is_readable.return_value = True
+    mock_get_genenset_metadata.return_value = geneset_metadata_w_pub_info
+
+    mock_pub_service_call.return_value = publication_by_id_resp
+
+    response = client.get("/api/genesets/1234/publication")
+
+    assert response.status_code == 200
+    assert response.json() == publication_by_id_resp
+
+
+@patch("geneweaver.api.services.geneset.get_geneset_metadata")
+@patch("geneweaver.api.services.geneset.is_geneset_readable_by_user")
+@patch("geneweaver.api.services.publications.get_publication")
+def test_publication_not_found_for_geneset(
+    mock_pub_service_call, mock_genset_is_readable, mock_get_genenset_metadata, client
+):
+    """Test get publication for a geneset with not found record."""
+    mock_genset_is_readable.return_value = True
+    mock_get_genenset_metadata.return_value = geneset_by_id_resp.get("geneset")
+    mock_pub_service_call.return_value = {"publication": None}
+
+    response = client.get("/api/genesets/1234/publication")
+
+    assert response.status_code == 404
