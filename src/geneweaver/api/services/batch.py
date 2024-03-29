157e7d8aa828698fbd70f2c58ba8e5d257d2ad07
Alexander Berger <alexander.berger@jax.org>
2024-03-29 12:46:43 -0400

    Removing commented code

--- src/geneweaver/api/services/batch.py
+++ src/geneweaver/api/services/batch.py
@@ -34,10 +34,6 @@ async def process_batch_file(
     for geneset in genesets:
         print(geneset, "\n")
 
-    # geneset_added, user_messages, system_messages = add_batch_genesets(
-    #     db, genesets, user_id
-    # )
-
     # TODO: Return the correct values.
     return [10], [], []
 
